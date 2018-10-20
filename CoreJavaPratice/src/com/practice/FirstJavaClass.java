c39ab7e2f3aa8f40402a81e4271c5d2c7207579d
sandeep <sandeep@sandeep-HP-Pavilion-g6-Notebook-PC>
2018-10-21 00:20:30 +0800

    added static class practice

--- CoreJavaPratice/src/com/practice/FirstJavaClass.java
+++ CoreJavaPratice/src/com/practice/FirstJavaClass.java
@@ -4,6 +4,7 @@ public class FirstJavaClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//added a new comment to check build
 		System.out.println("hello java");
 
 	}
--- CoreJavaPratice/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author sandeep
- *
- */
-module CoreJavaPratice {
-}
